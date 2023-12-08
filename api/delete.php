86c18a773a7f5e5e0bb1001141af0d861c3a1751
Piarre <pierre@ideestore.fr>
2023-12-08 21:32:26 +0100

    🔥 Remove useless session_start();

--- api/delete.php
+++ api/delete.php
@@ -1,12 +1,10 @@
 <?php
 
-session_start();
+isAuth();
 
 global $conn;
 require_once '../db.php';
 require_once '../auth.php';
-
-isAuth();
 
 $res = $conn->prepare("DELETE FROM users WHERE id = :id");
 $res->bindParam(':id', $_GET['id']);
