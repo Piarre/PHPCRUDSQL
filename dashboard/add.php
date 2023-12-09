769a1f60f91c46acebb416a8507ab45526fbe135
Piarre <pierre@ideestore.fr>
2023-12-09 18:38:21 +0100

    💄 Styling buttons

--- dashboard/add.php
+++ dashboard/add.php
@@ -8,25 +8,25 @@ isAuth();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
-    <title>Users list</title>
+    <title>Adding new user</title>
 </head>
 
-<body class="bg-gray-100 p-8">
-<div class="max-w-md mx-auto bg-white p-6 rounded-md shadow-md" id="userForm">
-    <form>
+<body class="bg-gray-200 p-8 pb-[150px] flex w-full h-screen w-full flex items-center justify-center">
+<div class="max-w-lg mx-auto bg-white p-6 rounded-md">
+    <form id="userForm">
         <h2 class="text-2xl font-bold mb-4">Add User</h2>
+        <div class="flex gap-10">
+            <div class="mb-4" id="name" style="width: 50%;">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
+                <input class="border-2 w-full border border-gray-300 p-2 rounded-md transition"
+                       type="text" name="name" id="name" placeholder="Name">
+            </div>
 
-        <div class="mb-4 " id="name">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="name">Name</label>
-            <input class="border-2 w-full border border-gray-300 p-2 rounded-md" type="text" name="name" id="name"
-                   placeholder="Name">
-
-        </div>
-
-        <div class="mb-4" id="surname">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="surname">Surname</label>
-            <input class="border-2 w-full border border-gray-300 p-2 rounded-md" type="text" name="surname"
-                   id="surname" placeholder="Surname">
+            <div class="mb-4" id="surname" style="width: 50%;">
+                <label class="block text-gray-700 text-sm font-bold mb-2" for="surname">Surname</label>
+                <input class="border-2 w-full border border-gray-300 p-2 rounded-md" type="text" name="surname"
+                       id="surname" placeholder="Surname">
+            </div>
         </div>
 
         <div class="mb-4" id="email">
@@ -43,10 +43,23 @@ isAuth();
                    id="password" placeholder="Password">
         </div>
 
-        <div class="mb-4" id="actif">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="actif">Actif</label>
-            <input class="border-2 w-full border border-gray-300 p-2 rounded-md" type="text" name="actif" id="actif"
-                   placeholder="Actif">
+        <div class="mb-4 w-full" id="enabled">
+            <label class="block text-gray-700 text-sm font-bold mb-2" id="text__enabled" for="enabled">Account
+                enabled</label>
+            <div class="flex gap-10 justify-center">
+                <button id="btn__enabled"
+                        class="bg-blue-500/50 hover:font-bold hover:-translate-y-1 hover:scale-110 transition duration-200 text-white font-bold py-2 px-4 rounded"
+                        type="button"
+                        disabled onclick="accountActivation()">
+                    Enabled
+                </button>
+                <button id="btn__disabled"
+                        class="bg-red-500 hover:-translate-y-1 hover:scale-110 hover:bg-red-700 transition duration-200 text-white font-bold py-2 px-4 rounded"
+                        type="button"
+                        onclick="accountActivation()">
+                    Disabled
+                </button>
+            </div>
         </div>
 
         <div class="mb-4" id="age">
@@ -58,9 +71,10 @@ isAuth();
         <span class="text-red-600 text-[14px] font-bold opacity-0" id="message">Please fill all fields</span>
         <div class="flex gap-10 px-4">
             <a type="button"
-               class="hover:cursor-pointer bg-blue-500 w-full text-white p-2 rounded-md transition text-center items-center""
+               class="hover:cursor-pointer hover:-translate-y-1 hover:scale-110 bg-blue-500 w-full text-white p-2 rounded-md transition text-center items-center""
             onclick="history.back()">Go Back</a>
-            <button class="bg-blue-500 w-full text-white p-2 rounded-md transition" type="submit" id="submit-btn">
+            <button class="bg-blue-500 hover:-translate-y-1 hover:scale-110 w-full text-white p-2 rounded-md transition"
+                    type="submit" id="submit-btn">
                 Add
             </button>
         </div>
@@ -108,6 +122,7 @@ isAuth();
             accountEnabled = true;
         }
     }
+
     function isEmailUsed(email) {
         fetch("/api/checkemail.php?email=" + email)
             .then((response) => response.json())
@@ -178,13 +193,11 @@ isAuth();
                     body: data,
                 })
 
-                window.location.href = "/dashboard";
+                window.location.href = `/dashboard?message=Added user ${name}?>`
             }
         },
         false,
     );
 </script>
-
 </body>
-
 </html>
--- dashboard/index.php
+++ dashboard/index.php
@@ -16,87 +16,164 @@ $users = $res->fetchAll();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
     <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+
+    <link rel="stylesheet" href="../main.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/notyf@3/notyf.min.css">
     <title>Users list</title>
 </head>
 <body class="bg-gray-100 p-8">
-
-    <div class="max-w-6xl mx-auto">
+<div class="max-w-6xl mx-auto">
+    <div class="flex justify-center gap-10 pb-4">
+        <a class="flex items-center gap-[5px]  bg-violet-500 transition hover:bg-violet-800 my-2 hover:-translate-y-1 font-bold hover:scale-110 text-xl text-white p-3 rounded-md"
+           href="/api/auth/logout.php">
+            Logout
+            <ion-icon name="log-out"></ion-icon>
+        </a>
         <h1 class="mb-4 text-4xl font-bold text-center text-gray-900 md:text-5xl lg:text-6xl">PHP SQL C.R.U.D.</h1>
-        <div class="flex justify-center gap-4">
-            <a class="bg-violet-500 transition hover:bg-violet-800 my-2  text-white p-2 rounded-md" href="/api/auth/logout.php">
-                Logout
-            </a>
-            <a class="bg-violet-500 transition hover:bg-violet-800 my-2 text-white p-2 rounded-md" href="/dashboard/add.php">
-                Add user
-            </a>
-        </div>
-        <table class="min-w-full bg-white border border-gray-300 rounded-md overflow-hidden">
-            <thead class="bg-gray-200">
-                <tr>
-                    <th class="py-2 px-4">ID</th>
-                    <th class="py-2 px-4">Name</th>
-                    <th class="py-2 px-4">Surname</th>
-                    <th class="py-2 px-4">Email</th>
-                    <th class="py-2 px-4">Age</th>
-                    <th class="py-2 px-4">Actif</th>
-                    <th class="py-2 px-4">Created by</th>
-                    <th class="py-2 px-4 w-full">Created at</th>
-                    <th class="py-2 w-full">Updated at</th>
-                    <th class="py-2 px-4">Actions</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php foreach ($users as $user): ?>
-                    <tr class="hover:bg-gray-300 transition">
-                        <td class="py-2 px-4">
-                            <?php echo $user['id']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['name']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['surname']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['email']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['age']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['actif']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['createdBy']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['createdAt']; ?>
-                        </td>
-                        <td class="py-2 px-4">
-                            <?php echo $user['updatedAt']; ?>
-                        </td>
-                        <td class="py-2 px-4 flex gap-4">
-                            <form action="../dashboard/update.php">
-                                <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-                                <button type="submit"
-                                    class="bg-blue-500 hover:bg-blue-700 transition duration-200 text-white font-bold py-2 px-4 rounded">
-                                    Update
-                                </button>
-                            </form>
-                            <form action="../api/delete.php">
-                                <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
-                                <button type="submit"
-                                        class="bg-rose-600 hover:bg-red-700 transition duration-200 text-white font-bold py-2 px-4 rounded">
-                                    Delete
-                                </button>
-                            </form>
-                    </tr>
-                <?php endforeach; ?>
-            </tbody>
-        </table>
-
+        <a class="flex items-center gap-[5px] bg-pink-500 transition hover:bg-pink-800 my-2 hover:-translate-y-1 font-bold hover:scale-110 text-xl text-white p-3 rounded-md"
+           href="/dashboard/add.php">
+            <ion-icon name="person-add"></ion-icon>
+            Add user
+        </a>
     </div>
+    <table id="users" class="order-column min-w-full bg-white border border-gray-300 rounded-md overflow-hidden">
+        <thead class="bg-gray-200">
+        <tr>
+            <th class="py-2 px-4">ID</th>
+            <th class="py-2 px-4">Name</th>
+            <th class="py-2 px-4">Surname</th>
+            <th class="py-2 px-4">Email</th>
+            <th class="py-2 px-4">Age</th>
+            <th class="py-2 px-4">enabled</th>
+            <th class="py-2 px-4">Created by</th>
+            <th class="py-2 px-4 w-full">Created at</th>
+            <th class="py-2 w-full">Updated at</th>
+            <th class="py-2 px-4">Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php foreach ($users as $user): ?>
+            <tr class="hover:bg-gray-300 transition">
+                <td class="py-2 px-4">
+                    <?php echo $user['id']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['name']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['surname']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['email']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['age']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php if ($user['enabled'] == 1) {
+                        echo "Enabled";
+                    } else {
+                        echo "Disabled";
+                    } ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['createdBy']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['createdAt']; ?>
+                </td>
+                <td class="py-2 px-4">
+                    <?php echo $user['updatedAt']; ?>
+                </td>
+                <td class="py-2 px-4 flex gap-4">
+                    <form action="../dashboard/update.php">
+                        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+                        <button type="submit"
+                                class="flex items-center gap-[5px] bg-blue-600 hover:bg-blue-700 transition duration-200 text-white text-lg font-bold p-3 rounded">
+                            <ion-icon name="at-circle"></ion-icon>
+                            Update
+                        </button>
+                    </form>
+                    <form action="../api/delete.php">
+                        <input type="hidden" name="id" value="<?php echo $user['id']; ?>">
+                        <button type="submit"
+                                class="flex items-center gap-[5px] bg-rose-600 hover:bg-red-700 transition duration-200 text-white text-lg font-bold p-3 rounded">
+                            <ion-icon name="trash-bin"></ion-icon>
+                            Delete
+                        </button>
+                    </form>
+            </tr>
+        <?php endforeach; ?>
+        </tbody>
+    </table>
+    <script>
+        var groupColumn = 5;
+        const usersApi = new DataTable('#users', {
+            initComplete: function () {
+                this.api()
+                    .columns()
+                    .every(function () {
+                        let column = this;
+                        let title = column.context[0].aoColumns[column.index()].sTitle;
+
+                        if (title !== 'Actions') {
+                            let input = document.createElement('input');
+                            input.placeholder = title;
+                            input.classList.add('p-2', 'w-[100px]', 'rounded-md', 'border', 'border-gray-300', 'focus:outline-none', 'focus:ring-2', 'focus:ring-violet-600', 'focus:border-transparent', 'transition');
+                            $(input).appendTo($(column.header()).empty())
+                                .on('keyup change clear', function () {
+                                    if (column.search() !== this.value) {
+                                        column
+                                            .search(this.value)
+                                            .draw();
+                                    }
+                                });
+                        }
+                    });
+            },
+            columnDefs: [{visible: false, targets: groupColumn}],
+            order: [[groupColumn, 'asc']],
+            displayLength: 25,
+            drawCallback: function () {
+                let api = this.api();
+                let rows = api.rows({page: 'current'}).nodes();
+                let last = null;
+
+                api.column(groupColumn, {page: 'current'}).data().each(function (group, i) {
+                    if (last !== group) {
+                        $(rows).eq(i).before(
+                            '<tr class="group bg-gray-200"><td colspan="10">' + group + '</td></tr>'
+                        );
+                        last = group;
+                    }
+                });
+            }
+        });
+
+        $('#users tbody').on('click', 'tr.group', function () {
+            var currentOrder = table.order()[0];
+            if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+                table.order([groupColumn, 'desc']).draw();
+            } else {
+                table.order([groupColumn, 'asc']).draw();
+            }
+        });
+
+        const notyf = new Notyf();
+
+        <?php if (isset($_GET['message'])): ?>
+        notyf.success('<?php echo $_GET['message']; ?>');
+        <?php endif; ?>
+    </script>
+</div>
 </body>
 
 </html>
